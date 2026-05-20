dc47d1567904bb96d3d3e80811c3271460da19fa
Chobotar Maksym <144207642+Sen1n@users.noreply.github.com>
2026-05-20 19:20:38 +0000

    Task 2.4 - Keyboard and mouse animation complete

--- main.cpp
+++ main.cpp
@@ -1,18 +1,52 @@
-
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
+
+#include "glad.h"
+#include <GLFW/glfw3.h>
+
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-int main() {
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "stb_image.h"
+// --- ШЕЙДЕРИ ---
+// Зверни увагу: у Vertex Shader додалася змінна uniform mat4 transform
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec2 aTexCoord;\n"
+    "out vec2 TexCoord;\n"
+    "uniform mat4 transform;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = transform * vec4(aPos, 1.0);\n"
+    "   TexCoord = vec2(aTexCoord.x, aTexCoord.y);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec2 TexCoord;\n"
+    "uniform sampler2D texture1;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = texture(texture1, TexCoord);\n"
+    "}\n\0";
+
+// Розміри вікна
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main()
+{
+    // Ініціалізація
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Task 2.4 - Keyboard and Mouse", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -25,10 +59,141 @@ int main() {
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window)) {
+    // Компіляція шейдерів
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Вершини прямокутника (розмір 1.0 на 1.0 в центрі)
+    float vertices[] = {
+        // позиції          // текстурні координати
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // верхній правий
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // нижній правий
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // нижній лівий
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // верхній лівий
+    };
+    unsigned int indices[] = {  
+        0, 1, 3, // перший трикутник
+        1, 2, 3  // другий трикутник
+    };
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Завантаження текстури (використовуємо твою картинку tex1.jpg)
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("tex1.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // --- ЗМІННІ ДЛЯ РУХУ ТА АНІМАЦІЇ ---
+    float rectX = 0.0f; // Позиція по X
+    float rectY = 0.0f; // Позиція по Y
+    float angle = 0.0f; // Кут обертання
+    
+    // Час для плавності (DeltaTime)
+    float deltaTime = 0.0f;
+    float lastFrame = 0.0f;
+
+    glUseProgram(shaderProgram);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Рахуємо дельта-час (щоб швидкість не залежала від потужності ПК)
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // --- 1. ОБРОБКА КЛАВІАТУРИ (рух стрілочками) ---
+        float cameraSpeed = 1.5f * deltaTime; 
+        if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+            rectX -= cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+            rectX += cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+            rectY += cameraSpeed;
+        if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+            rectY -= cameraSpeed;
+
+        // --- 2. ОБРОБКА МИШІ (наведення та обертання) ---
+        double mouseX, mouseY;
+        glfwGetCursorPos(window, &mouseX, &mouseY);
+        
+        // Переводимо координати миші (0..800, 0..600) в координати OpenGL (-1..1)
+        float ndcX = (mouseX / SCR_WIDTH) * 2.0f - 1.0f;
+        float ndcY = 1.0f - (mouseY / SCR_HEIGHT) * 2.0f; // Перевертаємо Y
+
+
+        if (ndcX >= rectX - 0.5f && ndcX <= rectX + 0.5f &&
+            ndcY >= rectY - 0.5f && ndcY <= rectY + 0.5f) {
+            
+
+            angle -= 100.0f * deltaTime; 
+        }
+
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glm::mat4 transform = glm::mat4(1.0f); // Одинична матриця
+        
+
+        transform = glm::translate(transform, glm::vec3(rectX, rectY, 0.0f));
+        transform = glm::rotate(transform, glm::radians(angle), glm::vec3(0.0f, 0.0f, 1.0f));
+
+        unsigned int transformLoc = glGetUniformLocation(shaderProgram, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+        // Малюємо
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
 
     glfwTerminate();
     return 0;
